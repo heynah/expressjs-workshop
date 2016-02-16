a42e6b4816e6e6a6a90cecbcbce2d38de1e07e72
Hanna Nicol <hannanicol@gmail.com>
2016-02-16 21:01:51 +0000

    Pass the results of a query (array) through a funtion that formats them to html and sends that

--- index.js
+++ index.js
@@ -1,10 +1,46 @@
 var express = require('express');
 var app = express();
 app.set('json spaces', 2);
+var Sequelize = require('sequelize');
+
+var mysql = require('mysql');
+
+var reddit = new Sequelize('reddit','heynah', '', {dialect: 'mysql'});
+
+/*var connection = mysql.createConnection({
+  host     : process.env.IP,
+  user     : process.env.C9_USER,
+  password : '',
+  database : 'reddit'
+});
+
+connection.query("SELECT Account.id, AddressBook.accountId, Account.email, AddressBook.name FROM Account LEFT JOIN AddressBook on Account.id=AddressBook.accountId", 
+function(err, rows){
+}*/
+
+var User = reddit.define('user', {
+    username: Sequelize.STRING,
+    screenName: Sequelize.STRING,
+    password: Sequelize.STRING});
+               //^ table,  feild definitions,table -- telling library about user table, knows ids!
+var Post = reddit.define('content', {
+    url: Sequelize.STRING,
+    title: Sequelize.STRING
+});
+var Vote = reddit.define('vote', {
+    upVote: Sequelize.BOOLEAN
+});
+// var Content = reddit.define('
+Post.belongsTo(User); //{foreignKey:'userId'});
+//                     // can connect children tables to parents *using Sequelizes defaults
+User.hasMany(Post);
+
+User.belongsToMany(Post, {through: Vote, as: "Upvotes"});
+Post.belongsToMany(User, {through: Vote});
+
 
 function op(operator, number1, number2) {
-  var opp = operator
-  console.log(opp)
+  var opp = operator;
   var result;
   switch (opp) {
     case 'add':
@@ -17,15 +53,45 @@ function op(operator, number1, number2) {
       result = number1/number2;
       break;
     case 'mult':
-      result = number1*number2;
+      result = (number1*number2).toFixed(2);
     /*  break
       default:
       result = "Unavailable. Please choose an operation 'add', 'sub', 'mult', or 'div' and two numbers";*/
   } 
-  console.log(result)
+  console.log(result);
   return result;
   
   }
+  
+function buildHtml(contentsArray){
+  var html = 
+  `<div id="contents">
+    <h1>List of contents</h1>
+      <ul class="contents-list">`
+  contentsArray.forEach(function(item){
+    html +=
+      `<li class="content-item">
+      <h2 class="content-item__title">
+        <a href="`+ item.url +`">`+ item.title + `</a>
+      </h2>
+      <p>Created by ` + item.user.username + `</p>
+      </li>`});
+      
+    html += `</ul></div>`;
+//console.log(html);
+return html;
+}
+
+function retrieveTop5(callback) {
+  Post.findAll({
+  order: [['createdAt','DESC']],
+  limit: 5,
+  include: User
+  }).then(function(res) {
+    callback(res);
+});
+
+}
 
 app.get('/hello/:firstName', function (req, res) {
   res.send("<h1>Hello " + req.params.firstName + ", you handsom devil, you!</h1>");
@@ -34,10 +100,19 @@ app.get('/hello/:firstName', function (req, res) {
 app.get('/op/:operation/:number1/:number2', function (request, response) {
   var operator = request.params.operation;
   var number1 = Number(request.params.number1);
-  var number2 = Number(request.params.number2)
+  var number2 = Number(request.params.number2);
   var total = op(operator,number1,number2);
   total ? response.json({"Operation": operator, "First number" : number1, "Second number": number2, "Solution" : total}) 
   : response.status(400).json({error: "Wrong, Buddy! Keep trying (enter an operation 'add', 'sub', 'mult', or 'div' and 2 numbers)."});
+});
+
+app.get('/contents', function(req, res) {
+  
+  retrieveTop5(function(contents){
+    var html = buildHtml(contents);
+    res.send(html);
+  });
+    
 });
 
 /* YOU DON'T HAVE TO CHANGE ANYTHING BELOW THIS LINE :) */
@@ -50,16 +125,6 @@ var server = app.listen(process.env.PORT, process.env.IP, function () {
   console.log('Example app listening at http://%s:%s', host, port);
 });
 
-/*app.get('/firstChar/:str', function(request, response) {  //dynamic endpoint :
-    var theRequestedString = request.params.str;    
-    var result = firstChar(theRequestedString);
-    response.send(result);
-}) 
-*/
 
-/*res.json([body])
-Sends a JSON response. This method is identical to res.send() with an object or array as the parameter. However, you can use it to convert other values to JSON, such as null, and undefined (although these are technically not valid JSON).
-
-res.json(null);
-res.json({ user: 'tobi' });
-res.status(500).json({ error: 'message' });*/
+// retrieveTop5(function(res){return res});
+retrieveTop5(function(res){console.log(res)});
